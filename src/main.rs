f4995e97db7d31525d317bbc59d0c47a2b0e071e
Harshdeep Kanhai <harshdeepkanhai@gmail.com>
2026-06-23 13:09:23 +0530

    Refactor temperature conversion logic and improve error handling in main function

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,12 @@ enum TemperatureUnit {
     Kelvin,
 }
 
+const KELVIN_OFFSET: f64 = 273.15;
+
 fn to_celsius(temp: f64, from_unit: &TemperatureUnit) -> f64 {
     match from_unit {
         TemperatureUnit::Fahrenheit => (temp - 32.0) * 5.0 / 9.0,
-        TemperatureUnit::Kelvin => temp - 273.15,
+        TemperatureUnit::Kelvin => temp - KELVIN_OFFSET,
         _ => temp,
     }
 }
@@ -18,7 +20,7 @@ fn to_celsius(temp: f64, from_unit: &TemperatureUnit) -> f64 {
 fn from_celsius(temp: f64, to_unit: &TemperatureUnit) -> f64 {
     match to_unit {
         TemperatureUnit::Fahrenheit => temp * 9.0 / 5.0 + 32.0,
-        TemperatureUnit::Kelvin => temp + 273.15,
+        TemperatureUnit::Kelvin => temp + KELVIN_OFFSET,
         _ => temp,
     }
 }
@@ -36,26 +38,17 @@ fn convert_to_unit(input_unit: char) -> Result<TemperatureUnit, String> {
     }
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("failed to read");
+    io::stdin().read_line(&mut input)?;
     let parts: Vec<&str> = input.split_whitespace().collect();
-    let temp = parts[0].parse::<f64>().expect("Error Parsing Temperature");
-    let from = match convert_to_unit(parts[1].parse::<char>().expect("Error Parsing Unit")) {
-        Ok(input_unit) => input_unit,
-        Err(msg) => {
-            println!("{}", msg);
-            return;
-        }
+    let [temp, from, to] = parts.as_slice() else {
+        return Err("Wrong number of inputs".into());
     };
-    let to: TemperatureUnit =
-        match convert_to_unit(parts[2].parse::<char>().expect("Error Parsing Unit")) {
-            Ok(input_unit) => input_unit,
-            Err(msg) => {
-                println!("{}", msg);
-                return;
-            }
-        };
-    let result = convert_temp(temp, &from, &to);
-    println!("{} {:?} =  {} {:?}", temp, from, result, to)
+    let from = convert_to_unit(from.parse::<char>()?)?;
+    let to: TemperatureUnit = convert_to_unit(to.parse::<char>()?)?;
+
+    let result = convert_temp(temp.parse::<f64>()?, &from, &to);
+    println!("{} {:?} =  {} {:?}", temp, from, result, to);
+    Ok(())
 }
